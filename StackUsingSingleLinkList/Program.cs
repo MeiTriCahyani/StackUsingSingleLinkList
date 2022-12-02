c7785835afcdabb5e63f70a4209bcee7be44930e
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-12-02 18:53:01 +0700

    add void display

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -44,6 +44,21 @@ namespace StackUsingSingleLinkList
             Console.WriteLine("\n The poped elements is: " + top.info);
             top = top.next; //make top point to the next node is squence
         }
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+
+
+                }
+            }
+        }
     }
     internal class Program
     {
