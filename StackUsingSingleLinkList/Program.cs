bbb7e9ba2505947e4b39e3909bb77ce2ed63a686
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-12-02 18:51:56 +0700

    add public void pop

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -39,6 +39,11 @@ namespace StackUsingSingleLinkList
             top = fresh;
             Console.WriteLine("\n" + element + "pushed");
         }
+        public void pop()
+        {
+            Console.WriteLine("\n The poped elements is: " + top.info);
+            top = top.next; //make top point to the next node is squence
+        }
     }
     internal class Program
     {
