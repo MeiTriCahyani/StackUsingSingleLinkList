40f11203fe07564436161c34316dc1021422774c
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-12-02 19:04:56 +0700

    add case

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -59,26 +59,47 @@ namespace StackUsingSingleLinkList
                 }
             }
         }
-    }
-    static void Main(string[] args)
-    {
-        Stack s = new Stack();
-        while (true)
+        static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.WriteLine("\n***Stack Menu***\n");
-            Console.WriteLine("1. Push");
-            Console.WriteLine("2. Pop");
-            Console.WriteLine("3. Display");
-            Console.WriteLine("4. Exit");
-            Console.Write("\nEnter your choice:");
-            string Input = Console.ReadLine();
-            char ch = Convert.ToChar(Input == "" ? "0" : Input);
-            switch (ch)
+            Stack s = new Stack();
+            while (true)
             {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice:");
+                string Input = Console.ReadLine();
+                char ch = Convert.ToChar(Input == "" ? "0" : Input);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number :");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
 
+                }
             }
         }
+
     }
-    
 }
